b1c527954c2983766fa6cdc7e8947fd60c193ef6
tbachvarova <teodora.sivkova@gmail.com>
2023-03-21 10:32:26 +0200

    added some DBFactory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,34 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'some descr text for 1'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'some descr text for 2'
-            ]
-        ];
-    }
-
-    public static function find($_id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing){
-            if($listing['id'] == $_id){
-                return $listing;
-            }
-        }
-
-    }
-
+class Listing extends Model
+{
+    use HasFactory;
 }
---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'tags' => 'laravel, php, api, backend',
+            //'tags' => fake()->randomElements(['laravel', 'php', 'api', 'backend']),
+            'company' => fake()->company(),
+            'email' => fake()->companyEmail(),
+            'website' => fake()->url(),
+            'location' => fake()->city(),
+            'description' => fake()->paragraph(5)
+        ];
+    }
+}
